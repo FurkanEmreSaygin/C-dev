c265ecbd40243ee91684f0732f0662b5189de530
furkan emre saygin <furkanemresaygi@gmail.com>
2025-02-13 18:14:03 +0300

    Kullanıcı Kartı Tasarlandı

---
+++ Kullanıcı Kartı.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.Write("Enter your surname: ");
+        string surname = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+       
+        Console.Write("Enter your city: ");
+        string city = Console.ReadLine();
+       
+        Console.Write("Enter your profession: ");
+        string profession = Console.ReadLine();
+
+        Console.WriteLine("----------------------------");
+        
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Surname: {surname}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"City: {city}");
+        Console.WriteLine($"Profession: {profession}");
+
+        Console.Write("----------------------------");
+    
+    
+    }
+}
--- obj/Debug/net9.0/C-dev.AssemblyInfo.cs
+++ obj/Debug/net9.0/C-dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e4549d3ae9cc2a93798b44c751e52ea8d58203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55ac1837d708e719cac904c2fb7e58369a55f89")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
